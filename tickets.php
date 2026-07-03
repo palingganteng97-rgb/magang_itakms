db13002385efd8de685e18a8501fe78f7e2eb7fb
Restu <palingganteng97@gmail.com>
2026-07-03 13:17:12 +0700

    Update tickets.php

--- tickets.php
+++ tickets.php
@@ -758,7 +758,4 @@ document.addEventListener("click", function(t) {
 });
 </script>
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?php include 'footer-admin.php'; ?>
